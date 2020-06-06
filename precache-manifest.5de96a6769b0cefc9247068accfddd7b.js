b5b05ceb1b159b1f6ac3071d41a75ae24977abf7
shashikanth369 <shashikanth258@gmail.com>
2020-06-06 01:57:08 -0400

    Updates

---
+++ precache-manifest.5de96a6769b0cefc9247068accfddd7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8e87310397e38581cd4e1562a4727b",
+    "url": "/kme-app/index.html"
+  },
+  {
+    "revision": "d9051129ef561cfd490e",
+    "url": "/kme-app/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "e8b03c16ffe23919f684",
+    "url": "/kme-app/static/css/main.ca051fb1.chunk.css"
+  },
+  {
+    "revision": "d9051129ef561cfd490e",
+    "url": "/kme-app/static/js/2.263fcd74.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kme-app/static/js/2.263fcd74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b03c16ffe23919f684",
+    "url": "/kme-app/static/js/main.54d775b0.chunk.js"
+  },
+  {
+    "revision": "6acad4b17651bca11546",
+    "url": "/kme-app/static/js/runtime-main.0dcbe6c1.js"
+  },
+  {
+    "revision": "e2f4743ffc474998f7ad539d14315f98",
+    "url": "/kme-app/static/media/Kisan Safe Gaurd.e2f4743f.png"
+  },
+  {
+    "revision": "ace3bf8324e942704876f48d82beccbb",
+    "url": "/kme-app/static/media/kme-logo.ace3bf83.png"
+  }
+]);
